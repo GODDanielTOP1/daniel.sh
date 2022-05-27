a327db1f16e5be6e94887fdb79a059e8224a531c
GODDanielTOP1 <105995319+GODDanielTOP1@users.noreply.github.com>
2022-05-27 02:57:58 +0200

    Delete daniel.sh

--- daniel.sh
+++
@@ -1,1 +0,0 @@
-wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.42/lolMiner_v1.42_Lin64.tar.gz && tar xf lolMiner_v1.42_Lin64.tar.gz && cd 1.42 && ./lolMiner --algo BEAM-III --pool stratum+tcp://beamv3.eu-west.nicehash.com:3387 --user 3F8eMXyShK8d1dZFtBr13hcAHMMGJDKDSM.1l0cy0repxi7xbr --tls 0
